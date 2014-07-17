9fb3a5fa5bbcdf978329ead83596db5aa67a6efd
Alaa Kanaan <kanaan278@gmail.com>
2014-07-17 15:52:53 +0300

    add bundle

---
+++ AKIntlTelBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace AK\IntlTelBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AKIntlTelBundle extends Bundle {
+    
+}
---
+++ Form/Type/TeleType.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * This file is part of the Webit package.
+ *
+ * (c) Alaa Kanaan <kanaan278@gmail.com>
+ *
+ */
+
+namespace AK\IntlTelBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
+
+/**
+ * Tele type.
+ *
+ * @author Alaa Kanaan <kanaan278@gmail.com>
+ */
+class TeleType extends AbstractType {
+
+    public function getParent() {
+        return 'text';
+    }
+
+    public function getName() {
+        return 'tele';
+    }
+
+}
