82f5b984468739c9d2f08ac430473b0a4f1dbf9d
SpikeO <mopdebeeck@gmail.com>
2014-10-03 00:18:19 +0200

    Update SpikeOIntlTelExtension.php

--- DependencyInjection/SpikeOIntlTelExtension.php
+++ DependencyInjection/SpikeOIntlTelExtension.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Webit\IntlTelBundle\DependencyInjection;
+namespace SpikeO\IntlTelBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
