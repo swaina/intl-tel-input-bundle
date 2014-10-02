928de47347592752c3779bf4cbfe39c95399467b
SpikeO <mopdebeeck@gmail.com>
2014-10-03 00:18:08 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Webit\IntlTelBundle\DependencyInjection;
+namespace SpikeO\IntlTelBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
